100c5a745bc379c856efe4c8c839599feef016ad
1946366627 <163810151+1946366627@users.noreply.github.com>
2024-11-20 02:12:24 +0800

    Add files via upload

---
+++ images.sh
@@ -0,0 +1,5 @@
+docker pull --platform amd64 hyperledger/fabric-peer:1.4.4
+docker pull --platform amd64 hyperledger/fabric-ca:1.4.4
+docker pull --platform amd64 hyperledger/fabric-orderer:1.4.4
+docker pull --platform amd64 hyperledger/fabric-tools:1.4.4
+docker pull --platform amd64 hyperledger/fabric-ccenv:1.4.4
