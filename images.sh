aaa0f2057582f1b1a69192d15bf0b012605958e5
xiao <163810151+1946366627@users.noreply.github.com>
2024-12-07 06:13:01 +0800

    Delete images.sh

--- images.sh
+++
@@ -1,5 +0,0 @@
-docker pull --platform amd64 hyperledger/fabric-peer:1.4.4
-docker pull --platform amd64 hyperledger/fabric-ca:1.4.4
-docker pull --platform amd64 hyperledger/fabric-orderer:1.4.4
-docker pull --platform amd64 hyperledger/fabric-tools:1.4.4
-docker pull --platform amd64 hyperledger/fabric-ccenv:1.4.4
